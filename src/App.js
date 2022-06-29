c2a01b4ac2a023775a3532e912f4aee064bbff7e
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-06-29 15:22:09 +0400

    improved contract and frontend

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
+import "./App.css";
 
-import './App.css';
-
-import { NavigationBar } from './components/navigation';
-import { Products } from './components/products';
-import { AddProduct } from './components/addproduct';
-
+import { NavigationBar } from "./components/navigation";
+import { Products } from "./components/products";
+import { AddProduct } from "./components/addproduct";
 
 import { useState, useEffect, useCallback } from "react";
-
 
 import Web3 from "web3";
 import { newKitFromWeb3 } from "@celo/contractkit";
 import BigNumber from "bignumber.js";
 
-
 import tech from "./contracts/tech.abi.json";
 import IERC from "./contracts/IERC.abi.json";
 
-
 const ERC20_DECIMALS = 18;
-
-
 
 const contractAddress = "0x50E058D38Aa084A77D5f166181913C1D443141e2";
 const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
-
-
 
 function App() {
   const [contract, setcontract] = useState(null);
@@ -33,8 +24,6 @@ function App() {
   const [kit, setKit] = useState(null);
   const [cUSDBalance, setcUSDBalance] = useState(0);
   const [products, setProducts] = useState([]);
-  
-
 
   const connectToWallet = async () => {
     if (window.celo) {
@@ -70,14 +59,12 @@ function App() {
     }
   }, [address, kit]);
 
-
-
   const getProducts = useCallback(async () => {
     const productsLength = await contract.methods.getProductsLength().call();
     const products = [];
     for (let index = 0; index < productsLength; index++) {
       let _products = new Promise(async (resolve, reject) => {
-      let product = await contract.methods.getProduct(index).call();
+        let product = await contract.methods.getProduct(index).call();
 
         resolve({
           index: index,
@@ -85,8 +72,8 @@ function App() {
           image: product[1],
           description: product[2],
           price: product[3],
-          noOfAvailable: product[4],
-          sold: product[5]   
+          stock: product[4],
+          sold: product[5],
         });
       });
       products.push(_products);
@@ -96,18 +83,11 @@ function App() {
     setProducts(_products);
   }, [contract]);
 
-
-  const addProduct = async (
-    _image,
-    _description,
-    _price,
-    _noOfAvailable
- 
-  ) => {
+  const addProduct = async (_image, _description, _price, _stock) => {
     let price = new BigNumber(_price).shiftedBy(ERC20_DECIMALS).toString();
     try {
       await contract.methods
-        .addProduct(_image, _description, price, _noOfAvailable)
+        .addProduct(_image, _description, price, _stock)
         .send({ from: address });
       getProducts();
     } catch (error) {
@@ -115,57 +95,21 @@ function App() {
     }
   };
 
-  const modifyPrice = async (_index, _price) => { 
+  const modifyPrice = async (_index, _price) => {
     const price = new BigNumber(_price).shiftedBy(ERC20_DECIMALS).toString();
     try {
       await contract.methods.modifyPrice(_index, price).send({ from: address });
       getProducts();
       alert("you have successfully changed the price");
-     
     } catch (error) {
       alert(error);
-    }};
+    }
+  };
 
-
-
-    const addInventory = async (
-      _index,
-      _ammount
-    ) => {
-      try {
-        await contract.methods
-          .addInventory(_index, _ammount)
-          .send({ from: address });
-        getProducts();
-      } catch (error) {
-        alert(error);
-      }
-    };
-
-    const reduceInventory = async (
-      _index,
-      _ammount
-   
-    ) => {
-      try {
-        await contract.methods
-          .reduceInventory(_index, _ammount)
-          .send({ from: address });
-        getProducts();
-      } catch (error) {
-        alert(error);
-      }
-    };
-  
-
-
-
-  const unlistProduct = async (
-    _index
-  ) => {
+  const addInventory = async (_index, _amount) => {
     try {
       await contract.methods
-        .unlistProduct(_index)
+        .addInventory(_index, _amount)
         .send({ from: address });
       getProducts();
     } catch (error) {
@@ -173,6 +117,25 @@ function App() {
     }
   };
 
+  const reduceInventory = async (_index, _amount) => {
+    try {
+      await contract.methods
+        .reduceInventory(_index, _amount)
+        .send({ from: address });
+      getProducts();
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const unlistProduct = async (_index) => {
+    try {
+      await contract.methods.unlistProduct(_index).send({ from: address });
+      getProducts();
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   const buyProduct = async (_index) => {
     try {
@@ -187,8 +150,8 @@ function App() {
       alert("you have successfully bought this image");
     } catch (error) {
       alert(error);
-    }};
-
+    }
+  };
 
   useEffect(() => {
     connectToWallet();
@@ -205,17 +168,20 @@ function App() {
       getProducts();
     }
   }, [contract, getProducts]);
-  
+
   return (
     <div className="App">
       <NavigationBar cUSDBalance={cUSDBalance} />
-      <Products products={products}
-       buyProduct={buyProduct}
-       walletAddress={address}
-       addInventory={addInventory}
-       reduceInventory={reduceInventory} 
-       modifyPrice={modifyPrice} />
-    
+      <Products
+        products={products}
+        buyProduct={buyProduct}
+        walletAddress={address}
+        addInventory={addInventory}
+        reduceInventory={reduceInventory}
+        modifyPrice={modifyPrice}
+        unlistProduct={unlistProduct}
+      />
+
       <AddProduct addProduct={addProduct} />
     </div>
   );
--- src/contracts/tech.sol
+++ src/contracts/tech.sol
@@ -3,116 +3,163 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 interface IERC20Token {
-  function transfer(address, uint256) external returns (bool);
-  function approve(address, uint256) external returns (bool);
-  function transferFrom(address, address, uint256) external returns (bool);
-  function totalSupply() external view returns (uint256);
-  function balanceOf(address) external view returns (uint256);
-  function allowance(address, address) external view returns (uint256);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+    function transfer(address, uint256) external returns (bool);
+
+    function approve(address, uint256) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external returns (bool);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function allowance(address, address) external view returns (uint256);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 }
 
 contract TechProduct {
-
-
-    uint internal productsLength = 0;
-    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+    uint256 private productsLength = 0;
+    address private cUsdTokenAddress =
+        0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
     struct Product {
         address payable owner;
         string image;
         string description;
-        uint price;
-        uint noOfAvailable;
-        uint sold;
+        uint256 price;
+        uint256 stock;
+        uint256 sold;
     }
 
-    mapping (uint => Product) internal products;
+    mapping(uint256 => Product) private products;
+
+    mapping(uint256 => bool) private deleted;
 
 
-// add a new product
+    event AddProduct(uint index, address owner);
+    event DeleteProduct(uint index);
+    event AddInventory(uint index, uint amount, uint total);
+    event ReduceInventory(uint index, uint amount, uint total);
+
+    modifier checkOwner(uint256 _index) {
+        require(
+            msg.sender == products[_index].owner,
+            "only product owner can perform this action"
+        );
+        _;
+    }
+
+    modifier isDeleted(uint _index){
+        require(!deleted[_index], "Product has already been deleted");
+        _;
+    }
+
+    // add a new product
     function addProduct(
         string memory _image,
-        string memory _description, 
-        uint _price,
-        uint _noOfAvailable
+        string memory _description,
+        uint256 _price,
+        uint256 _stock
     ) public {
-        uint _sold = 0;
+        require(bytes(_image).length > 7, "Enter a valid image uri"); // ipfs links starts with ipfs://
+        require(bytes(_description).length > 0, "Enter a valid description");
+        require(_price > 0, "Enter a valid price");
+        require(_stock > 0, "Enter a valid inventory number");
+        uint256 _sold = 0;
         products[productsLength] = Product(
             payable(msg.sender),
             _image,
             _description,
             _price,
-            _noOfAvailable,
+            _stock,
             _sold
         );
+        emit AddProduct(productsLength, msg.sender);
         productsLength++;
     }
 
-      // unlisting a product  from the marketplace
-        function unlistProduct(uint _index) external {
-	        require(msg.sender == products[_index].owner, "can't delete picture");         
-            products[_index] = products[productsLength - 1];
-            delete products[productsLength - 1];
-            productsLength--; 
-	 }
-
-
-// add more inventory
-    function addInventory(uint _index, uint _ammount) external{
-        require(msg.sender == products[_index].owner, "only owner can perform transaction");
-        require(_ammount != 0 , "only owner can perform transaction");
-        products[_index].noOfAvailable = products[_index].noOfAvailable + _ammount;
+    // unlisting a product  from the marketplace
+    function unlistProduct(uint256 _index) external checkOwner(_index) isDeleted(_index) {
+        require(_index < productsLength, "Query of non existant product");
+        delete products[_index];
+        deleted[_index] = true;
+        emit DeleteProduct(_index);
     }
 
-// reduce inventory
-    function reduceInventory(uint _index, uint _ammount) external{
-        require(msg.sender == products[_index].owner, "only owner can perform transaction");
-        require(_ammount < products[_index].noOfAvailable, "only owner can perform transaction");
-        products[_index].noOfAvailable = products[_index].noOfAvailable - _ammount;
+    // add more inventory
+    function addInventory(uint256 _index, uint256 _amount)
+        external
+        isDeleted(_index) checkOwner(_index) 
+    {
+        require(_amount > 0, "Enter a valid amount for restocking");
+        products[_index].stock = products[_index].stock + _amount;
+        emit AddInventory(_index, _amount, products[_index].stock);
     }
 
+    // reduce inventory
+    function reduceInventory(uint256 _index, uint256 _amount)
+        external
+        isDeleted(_index) checkOwner(_index)
+    {
+        require(
+            _amount < products[_index].stock,
+            "only owner can perform transaction"
+        );
+        products[_index].stock = products[_index].stock - _amount;
+        emit ReduceInventory(_index, _amount, products[_index].stock);
+    }
 
-// getting product
-    function getProduct(uint _index) public view returns (
-        address payable, 
-        string memory, 
-        string memory, 
-        uint, 
-        uint,
-        uint
-    ) {
+    // getting product
+    function getProduct(uint256 _index)
+        public
+        view
+        returns (
+            address payable,
+            string memory,
+            string memory,
+            uint256,
+            uint256,
+            uint256
+        )
+    {
         return (
             products[_index].owner,
             products[_index].image,
             products[_index].description,
             products[_index].price,
-            products[_index].noOfAvailable,
+            products[_index].stock,
             products[_index].sold
-          
         );
     }
-
 
     //buying a product
-    function buyProduct(uint _index) public payable  {
-        require(products[_index].noOfAvailable > 0, "Sold out");
+    function buyProduct(uint256 _index) public payable isDeleted(_index) {
+        require(msg.sender != products[_index].owner, "You can't buy your own products");
+        require(products[_index].stock > 0, "Sold out");
         require(
-          IERC20Token(cUsdTokenAddress).transferFrom(
-            msg.sender,
-            products[_index].owner,
-            products[_index].price
-          ),
-          "Transfer failed."
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                products[_index].owner,
+                products[_index].price
+            ),
+            "Transfer failed."
         );
         products[_index].sold++;
-        products[_index].noOfAvailable--;
+        products[_index].stock--;
     }
 
-    
     // to get the length of products in the mapping
-    function getProductsLength() public view returns (uint) {
+    function getProductsLength() public view returns (uint256) {
         return (productsLength);
     }
 }
